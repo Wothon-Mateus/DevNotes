8d1c81bdcccbb43cc9be2bcc38c23f04e8028d64
Wothon-Mateus <13wothon@gmail.com>
2025-09-02 19:41:03 -0300

    inicio do JS

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Elementos
+const notesContainer = document.querySelector("#notes-container");
+
+const noteInput = document.querySelector("#note-content");
+
+const addNoteBtn = document.querySelector(".add-note");
+
+const searchInput = document.querySelector("#search-input");
+
+const exportBtn = document.querySelector("#export-notes");
